9dde9331f2323d71d8c74b05bada51e83f7dc26c
Borruso <gborruso@dinamicheaziendali.it>
2024-02-13 10:40:20 +0100

    [FIX] l10n_it_asset_management: fix default company

--- l10n_it_asset_management/wizard/account_move_manage_asset.py
+++ l10n_it_asset_management/wizard/account_move_manage_asset.py
@@ -13,7 +13,7 @@ class WizardAccountMoveManageAsset(models.TransientModel):
 
     @api.model
     def get_default_company_id(self):
-        return self.env.user.company_id
+        return self.env.company
 
     @api.model
     def get_default_move_ids(self):
