5f4c53f44f1cea4c577e89b62dafecabb60d3f4f
Dinamiche Aziendali <52443494+DinamicheAziendali@users.noreply.github.com>
2024-04-11 12:50:01 +0200

    Update post-migration.py

--- l10n_it_delivery_note/migrations/16.0.1.1.0/post-migration.py
+++ l10n_it_delivery_note/migrations/16.0.1.1.0/post-migration.py
@@ -3,6 +3,7 @@ from openupgradelib import openupgrade  # pylint: disable=W7936
 
 @openupgrade.migrate()
 def migrate(env, version):
+    return
     carriers = env["stock.delivery.note"].search([]).mapped("carrier_id")
     if carriers:
         carriers.write({"is_carrier": True})
